0b8e1e807514e8f699073199fea50c009c14e8d1
qaiik <82181231+qaiik@users.noreply.github.com>
2022-06-02 00:25:28 -0600

    Create warningLog.js

---
+++ ps/lib/warningLog.js
@@ -0,0 +1,4 @@
+console.log('%c%s', 'color: white; font-size: 100px;text-shadow: 0px 0px 20px black;', 'Wait lol!');
+console.log('%c%s', 'color: white; font-size: 25px;text-shadow: 0px 0px 5px red;', "If someone told you to copy/paste something here, high chance you're being scammed.");
+console.log('%c%s', 'color: white; font-size: 35px;text-shadow: 0px 0px 10px darkred;', 'Pasting anything in here could give attackers access to your Betastar account.');
+console.log('%c%s', 'color: white; font-size: 25px;text-shadow: 0px 0px 5px skyblue;', 'Visit our discord at https://discord.gg/FzcCsGPVQ6.');
